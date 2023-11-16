1213f2e0bae42018ca2d4bacad7932a1a8f78214
brianreyna <147009426+brianreyna@users.noreply.github.com>
2023-11-16 00:05:54 -0800

    updated a couple of lines
    Was thinking we could have the drop down menu be able to select the various storylines. It would close the current frame and then open a new one for the storyline.

--- src/awp.java
+++ src/awp.java
@@ -1,65 +1,66 @@
-package FinalProjectPractice;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Objects;
 import javax.swing.*;
 
 public class awp extends Frame
 {
-      public static void main(String[] args) 
+    public static void main(String[] args)
     {
         Button b;
-        Choice c = new Choice(); 
+        Choice c = new Choice();
         JFrame f;
         f = new JFrame("Choose Your Own Adventure");
         b = new Button("Click Here to Begin");
         b.setBounds(400, 400, 200, 200);
-        b.addActionListener(new ActionListener() 
+        b.addActionListener(new ActionListener()
         {
-            public void actionPerformed(ActionEvent e) 
+            public void actionPerformed(ActionEvent e)
             {
+                c.add("Pick Your Adventure!");
                 c.add("Zamiel's Storyline");
-                c.add("Emilys Storyline");
+                c.add("Emily's Storyline");
                 c.add("Arju's Storyline");
                 c.add("Brian's Storyline");
                 c.add("Z's Storyline");
                 c.add("Chris's Storyline");
                 f.add(c);
 
-                c.addItemListener(new ItemListener() 
+                c.addItemListener(new ItemListener()
                 {
                     public void itemStateChanged(ItemEvent e)
                     {
-                        if (c.getSelectedItem() == "Zamiel's Storyline")
+                        if (Objects.equals(c.getSelectedItem(), "Zamiel's Storyline"))
                         {
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                             // go to Zamiel's Storyline
-                            System.out.println("Zamiel");
                         }
-                        else if (c.getSelectedItem() == "Emily's Storyline")
+                        else if (Objects.equals(c.getSelectedItem(), "Emily's Storyline"))
                         {
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                             // go to Emily's Storyline
-                            System.out.println("Emily");
                         }
-                        else if (c.getSelectedItem() == "Arju's Storyline")
+                        else if (Objects.equals(c.getSelectedItem(), "Arju's Storyline"))
                         {
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                             // go to Arju's Storyline
-                            System.out.println("Arju");
                         }
-                        else if (c.getSelectedItem() == "Brian's Storyline")
+                        else if (Objects.equals(c.getSelectedItem(), "Brian's Storyline"))
                         {
-                            // go to Brian's Storyline
-                            System.out.println("Brian");
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+                            BrianStoryline.main(args);
                         }
-                        else if (c.getSelectedItem() == "Z's Storyline")
+                        else if (Objects.equals(c.getSelectedItem(), "Z's Storyline"))
                         {
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                             // go to Z's Storyline
-                            System.out.println("Z");
                         }
                         else
                         {
+                            f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
                             // go to Chris's Storyline
-                            System.out.println("Chris");
                         }
-                        
+
                     }
                 });
             }
