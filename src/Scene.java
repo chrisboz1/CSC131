47bd991b854eee2bd6883bc9bee8703965952c27
ZamielG <zgripo2@csus.edu>
2023-11-18 18:03:01 -0800

    Scene class compete/close to finished.
    Removed ID. Multiple functions created to streamline the creation of
    Scene objects and their JSwing components.

--- src/Scene.java
+++ src/Scene.java
@@ -1,68 +1,252 @@
+import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Scene 
 {
-	private String id;
 	private JFrame frame;
 	private Scene[] nextScenes;
 	
 	public Scene() 
 	{
-		this.id = "-1";
 		this.frame = new JFrame();
+		frame.setBackground(new Color(0, 0, 0));
 		this.nextScenes = null;
 	}
 	
-	public Scene(String id, JFrame frame, Scene[] nextScenes) 
+	public Scene(JFrame frame, Scene[] nextScenes) 
 	{
-		this.id = id;
 		this.frame = frame;
 		this.nextScenes = nextScenes;
 	}
-
-	public String getID() 
-	{
-		return id;
-	}
-
+	
 	public JFrame getFrame() 
 	{
 		return frame;
 	}
 	
-	public Scene getNextScene(int choice) 
+	public void advanceScene(int choice) 
 	{
-		return nextScenes[choice];
+		this.frame.setVisible(false);
+		nextScenes[choice].getFrame().setVisible(true);
 	}	
 	
-	public void createSceneFrame(String imageName) 
+	public void setNextScenes(Scene[] nextScenes) 
 	{
-		System.out.println(System.getProperty("user.dir"));
+		this.nextScenes = nextScenes;
+	}
+	
+	//Functions that simplify adding JSwing components
+	
+	public ImageIcon addImage(String imageName) 
+	{
+		File file = new File(".\\src\\resources\\" + imageName);
+		if (!file.exists()) 
+		{
+			System.out.println("Invalid Image File.");
+			return null;
+		}
+		
 		ImageIcon icon1 = new ImageIcon(".\\src\\resources\\" + imageName);
 		frame.add(new JLabel(icon1));
-		
-		frame.setBounds(0, 0, icon1.getIconWidth(), icon1.getIconHeight());
-		frame.setResizable(false);
+		return icon1;
 	}
 	
-	public void createSceneFrame(String imageName, String[] choices) 
+	public void addDescription(String description) 
+	{
+		JTextArea text = new JTextArea();
+		text.setText(description);
+		text.setLineWrap(true);
+		text.setOpaque(false);
+		text.setBounds(frame.getWidth()/2, 0, frame.getWidth()-50, frame.getHeight());
+		text.setEditable(false);
+		
+		frame.add(text);
+		frame.setBounds(0, 0, frame.getWidth(), frame.getHeight()+100);
+	}
+	
+	public void addChoices(String[] choices) 
+	{
+		Choice choiceList = new Choice(); //Create user choices
+		for (int i = 0; i < choices.length; i++) 
+		{
+			choiceList.add(choices[i]);
+		}
+		
+		choiceList.addItemListener(new ItemListener() //Give choices functionality
+        {
+            public void itemStateChanged(ItemEvent e)
+            {
+            	String[] listChoices = choices;
+            	for (int i = 1; i < choices.length; i++) 
+        		{
+	                if (Objects.equals(choiceList.getSelectedItem(), listChoices[i]))
+	                {
+	                    advanceScene(i-1);
+	                }
+	            }
+            }
+        });
+		
+		frame.add(choiceList);
+		frame.setBounds(0, 0, frame.getWidth(), frame.getHeight()+100);
+	}
+	
+	//createSceneFrame functions are optional functions to make it easier to create a whole frame
+	
+	/**
+     * Creates a scene frame from an image.
+     *
+     * @param imageName  The image file name in the resources folder.
+     */
+	public void createSceneFrame(String imageName) 
+	{
+		frame.setLayout(new FlowLayout()); //Set layout for all elements
+		
+		ImageIcon icon = addImage(imageName);
+		
+		if (icon != null) 
+		{
+			frame.setBounds(0, 0, icon.getIconWidth(), icon.getIconHeight());
+		}
+		else 
+		{
+			frame.setBounds(0, 0, 500, 400);
+		}
+	}
+	
+	/**
+     * Creates a scene frame from an image and a description. Use an invalid
+     * file name if you wish to have just the description.
+     *
+     * @param imageName		The image file name in the resources folder.
+     * @param description	Description of the scene.
+     */
+	public void createSceneFrame(String imageName, String description) 
 	{
 		createSceneFrame(imageName);
-		//Being worked on
-		
+		addDescription(description);
 	}
 	
-	public static void main(String[] args) throws IOException 
+	/**
+     * Creates a scene frame from an image and String array of choices. Use if next scenes
+     * are already defined.
+     *
+     * @param imageName	The image file name in the resources folder.
+     * @param choices	Choices displayed to the player. First string is used as a header.
+     */
+	public void createSceneFrame(String imageName, String[] choices)
 	{
-		Scene scene = new Scene();
-		scene.createSceneFrame("hors.jpg");
+		createSceneFrame(imageName); //Create frame with selected image
+		addChoices(choices);
+	}
+	
+	/**
+     * Creates a scene frame from an image, a description, and a String array of choices. 
+     * Use if next scenes are already defined.
+     *
+     * @param imageName	The image file name in the resources folder.
+     * @param description	Description of the scene.
+     * @param choices	Choices displayed to the player. First string is used as a header.
+     */
+	public void createSceneFrame(String imageName, String description, String[] choices)
+	{
+		createSceneFrame(imageName, description); //Create frame with selected image and description
+		addChoices(choices);
+	}
+	
+	/**
+     * Creates a scene frame from an image and String array of choices. Use if next scenes
+     * are not already defined.
+     *
+     * @param imageName		The image file name in the resources folder.
+     * @param choices		Choices displayed to the player. First string is used as a header.
+     * @param newNextScenes	The scene objects associated with each choice.
+     */
+	public void createSceneFrame(String imageName, String[] choices, Scene[] newNextScenes) 
+	{
+		createSceneFrame(imageName, choices);
+		setNextScenes(newNextScenes);
+	}
+	
+	/**
+     * Creates a scene frame from an image, a description, and a String array of choices.
+     * Use if next scenes are not already defined.
+     *
+     * @param imageName		The image file name in the resources folder.
+     * @param choices		Choices displayed to the player. First string is used as a header.
+     * @param newNextScenes	The scene objects associated with each choice.
+     */
+	public void createSceneFrame(String imageName, String description, String[] choices, Scene[] newNextScenes) 
+	{
+		createSceneFrame(imageName, description, choices);
+		setNextScenes(newNextScenes);
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException 
+	{
+		test2();
+	}
+	
+	private static void test1() throws InterruptedException 
+	{
+		Scene scene1 = new Scene();
+		scene1.createSceneFrame("hors.jpg");
 		
-		scene.getFrame().setVisible(true);
+		Scene scene2 = new Scene();
+		scene2.createSceneFrame("image.png");
+		
+		Scene scene3 = new Scene();
+		scene3.createSceneFrame("zeedee_by_thebirdgang_df81fqi-pre.jpg");
+		
+		Scene scenes1[] = {scene2, scene3};
+		
+		scene1.setNextScenes(scenes1);
+		
+		scene1.getFrame().setVisible(true);
+		TimeUnit.SECONDS.sleep(5);
+		scene1.advanceScene(0);
+	}
+	
+	private static void test2() throws InterruptedException 
+	{
+		//Declare Scenes
+		// 1 is parent of 2 and 3
+		Scene scene1 = new Scene();
+		Scene scene2 = new Scene();
+		Scene scene3 = new Scene();
+		
+		// 2 is parent of 4 and 5
+		Scene scene4 = new Scene();
+		Scene scene5 = new Scene();
+		
+		//Declare choices and next scenes
+		String choices1[] = {"This choice shouldnt do anything", "the second coming", "Halo 3"};
+		Scene scenes1[] = {scene2, scene3};
+		
+		String choices2[] = {"Some more scenes", "the 4th", "the 5 guys burgers and fries"};
+		Scene scenes2[] = {scene4, scene5};
+		
+		//Create scene frames and associate choices
+		scene1.createSceneFrame("hors.jpg", choices1, scenes1);
+		scene2.createSceneFrame("null", "this scene has no image, but it has words. How are you, "
+									  + "by the way? I am doing quite fine myself. I am just testing out how far I can push the text here"
+									  + "and all that jazz. oh boy howdy.", choices2, scenes2);
+		scene3.createSceneFrame("zeedee_by_thebirdgang_df81fqi-pre.jpg", "I know what you did.");
+		
+		scene4.createSceneFrame("empty", "what am i?");
+		scene5.createSceneFrame("image.png", "WHAT AM I FIGHTING FOOOOOORRRR");
+		
+		scene1.getFrame().setVisible(true);
 	}
 }
