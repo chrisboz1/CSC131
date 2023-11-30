1271d9820435df44f0669fcad7fa4c62cc982e6c
emileymynhier1 <147010784+emileymynhier1@users.noreply.github.com>
2023-11-29 18:58:28 -0800

    Update EmilyStoryLine.java

--- src/EmilyStoryLine.java
+++ src/EmilyStoryLine.java
@@ -1,1 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EmilyStoryLine {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cat Adoption Storyline");
+        JLabel label = new JLabel("<html>You enter a room with two adorable cats.<br>One is black and the other is orange.<br>Which one will you choose?</html>");
+        label.setBounds(100, 50, 400, 100);
+        frame.add(label);
+
+        JButton blackCatButton = new JButton("Choose the Black Cat");
+        JButton orangeCatButton = new JButton("Choose the Orange Cat");
+        blackCatButton.setBounds(100, 150, 200, 50);
+        orangeCatButton.setBounds(300, 150, 200, 50);
+
+        blackCatButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                chooseCat("Black");
+            }
+        });
+
+        orangeCatButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                chooseCat("Orange");
+            }
+        });
+
+        frame.add(blackCatButton);
+        frame.add(orangeCatButton);
+        frame.setSize(600, 300);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    private static void chooseCat(String color) {
+        JFrame nameFrame = new JFrame("Name Your " + color + " Cat");
+        JLabel nameLabel = new JLabel("What will you name your " + color.toLowerCase() + " cat?");
+        nameLabel.setBounds(50, 50, 300, 50);
+        nameFrame.add(nameLabel);
+
+        JTextField nameField = new JTextField();
+        nameField.setBounds(50, 100, 200, 50);
+        nameFrame.add(nameField);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(50, 160, 100, 50);
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String catName = nameField.getText();
+                JOptionPane.showMessageDialog(nameFrame, "Congratulations! Your " + color.toLowerCase() + " cat is now named " + catName + "!");
+            }
+        });
+
+        nameFrame.add(submitButton);
+        nameFrame.setSize(400, 300);
+        nameFrame.setLayout(null);
+        nameFrame.setVisible(true);
+    }
+}
 
