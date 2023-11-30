45921d91d667332b6f1fe525c38fe0343a024ad0
brianreyna <147009426+brianreyna@users.noreply.github.com>
2023-11-29 18:27:22 -0800

    added more branches to story

--- src/BrianStoryline.java
+++ src/BrianStoryline.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.beans.PropertyChangeListener;
 import java.util.Objects;
 import javax.swing.*;
 
@@ -34,8 +35,43 @@ public class BrianStoryline extends Frame
 
                 JButton rowAway = new JButton("Take the boat and row off the island");
                 JButton treasureMap1 = new JButton("Take the map and find the hidden treasure");
-                rowAway.setBounds(100, 550, 200, 50);
-                treasureMap1.setBounds(375, 550, 200, 50);
+                rowAway.setBounds(150, 550, 250, 50);
+                treasureMap1.setBounds(525, 550, 275, 50);
+
+                rowAway.addActionListener(new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        rowBoat.setVisible(false);
+                        JFrame rowedOut = new JFrame("Row Out to Sea Scene");
+
+                        JLabel rolb = new JLabel("<html>You rowed off the island and were killed by a shark!</html>");
+                        rolb.setBounds(350, 200, 300, 300);
+                        rowedOut.add(rolb);
+
+                        rowedOut.setSize(1000, 1000);
+                        rowedOut.setLayout(null);
+                        rowedOut.setVisible(true);
+                    }
+                });
+
+                treasureMap1.addActionListener(new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        rowBoat.setVisible(false);
+                        JFrame hiddenTreasure1 = new JFrame("Hidden Treasure Scene");
+
+                        JLabel ft1lb = new JLabel("<html>You went out to find the hidden treasure.<br>You found the hidden treasure and were rescued by a helicopter!</html>");
+                        ft1lb.setBounds(350, 200, 300, 300);
+                        hiddenTreasure1.add(ft1lb);
+
+                        hiddenTreasure1.setSize(1000, 1000);
+                        hiddenTreasure1.setLayout(null);
+                        hiddenTreasure1.setVisible(true);
+
+                    }
+                });
 
                 rowBoat.add(rowAway);
                 rowBoat.add(treasureMap1);
@@ -52,8 +88,17 @@ public class BrianStoryline extends Frame
                 f.setVisible(false);
                 JFrame wreckedHelicopter = new JFrame("Wrecked Helicopter Scene");
 
+                JLabel whlb = new JLabel("<html>Inside the helicopter you find two maps, one map seems to lead to a campsite somewhere on the island.<br>You also notice there is another map that leads to a secret laboratory somewhere beneath the island<br>Which map will you take and follow?</html>");
+                whlb.setBounds(350, 200, 300, 300);
+                wreckedHelicopter.add(whlb);
 
+                JButton campsite = new JButton("Take the map and find the campsite");
+                JButton lab = new JButton("Take the other map and find the secret lab");
+                campsite.setBounds(150, 550, 300, 50);
+                lab.setBounds(525, 550, 300, 50);
 
+                wreckedHelicopter.add(campsite);
+                wreckedHelicopter.add(lab);
                 wreckedHelicopter.setSize(1000, 1000);
                 wreckedHelicopter.setLayout(null);
                 wreckedHelicopter.setVisible(true);
@@ -67,8 +112,17 @@ public class BrianStoryline extends Frame
                 f.setVisible(false);
                 JFrame tent = new JFrame("Tent Scene");
 
+                JLabel tlb = new JLabel("<html>Inside of the tent you discover a map that leads to a hidden treasure somewhere on the island.<br>You also notice there are smoke signals in the tent.<br>Which item will you take and use?</html>");
+                tlb.setBounds(350, 200, 300, 300);
+                tent.add(tlb);
 
+                JButton smokeSignal = new JButton("Take the smoke signals and try to escape the island");
+                JButton treasureMap2 = new JButton("Take the map and find the hidden treasure");
+                smokeSignal.setBounds(100, 550, 325, 50);
+                treasureMap2.setBounds(550, 550, 300, 50);
 
+                tent.add(smokeSignal);
+                tent.add(treasureMap2);
                 tent.setSize(1000, 1000);
                 tent.setLayout(null);
                 tent.setVisible(true);
