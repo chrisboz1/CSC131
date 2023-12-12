0732990a88496fa6dc9bda84e5ed786a2eb4fdd3
ZamielG <zgripo2@csus.edu>
2023-12-11 18:09:07 -0800

    Crystal storyline expanded.

--- src/CrystalStoryline.java
+++ src/CrystalStoryline.java
@@ -1,11 +1,12 @@
 import java.awt.Color;
 
+//Zamiel's Storyline
 public class CrystalStoryline 
 {
 	public static void main(String[] args) 
 	{
 		String folderPath = "crystal_storyline/";
-		Color caveColor = new Color(9, 3, 49);
+		
 		
 		//Intro Scenes and Branches
 		Scene intro1 = new Scene();
@@ -108,17 +109,30 @@ public class CrystalStoryline
 		Scene fall1 = new Scene();
 		Scene fall2 = new Scene();
 		Scene undertale = new Scene();
+		Scene device = new Scene();
+		Scene ambush = new Scene();
 		
+		Scene ambushFail = new Scene();
+		Scene ambushWin = new Scene();
+		
+		Scene ambushBad = new Scene();
+		
+		Scene ambushWin1 = new Scene();
+		Scene ambushWin2 = new Scene();
+		Scene downWith = new Scene();
+		Scene imminentBoom = new Scene();
+		Scene orbSuccess = new Scene();
+		Scene orbSuccess1 = new Scene();
 		//Caught
 		Scene withThem = new Scene();
 		Scene fleeBetrayal = new Scene();
 		Scene fleeBad = new Scene();
 		
 		//Flee Route Choices
-		String fleeDesc = "";
+		String fleeDesc = "REACT FAST TO FLEE SUCCESSFULLY";
 		String fleeChoices[] = { "...", "!" };
 		Scene fleeScenes[] = {fleeHidden, fleeCaught};
-		flee.addAutoAdvance(5, 1);
+		flee.addAutoAdvance(2, 1);
 		
 		//Hidden
 		String fleeHiddenDesc = "You evade the enemy patrol, dashing aimlessly through this white desert. Fittingly, like quicksand"
@@ -134,12 +148,71 @@ public class CrystalStoryline
 		fall2.setNextScenes(fall2Next);
 		fall2.addAutoAdvance(2, 0);
 		
-		undertale.getFrame().setBackground(caveColor);
 		String undertaleDesc = "You've survived worse. This is a strange cave you found yourself in; it doesn't seem to be a natural"
-							 + " formation. ";
-		String undertaleChoices[] = { "Uh oh.", "Brace for impact." };
-		Scene undertaleScenes[] = {fall1};
+							 + " formation. You hear distant sounds of machinery echo through the tunnels.";
+		String undertaleChoices[] = { "Must look into this.", "Explore the cave." };
+		Scene undertaleScenes[] = {device};
 		
+		String deviceDesc = "As you walk down one of the tunnels, the orb stirs once again. The source of the disruption becomes clear"
+						  + ", as you bear witness to a malevolent device that you eventually come across.";
+		String deviceChoices[] = { "The accursed thing stands in the way.", "Destroy this Crystal Imperial contraption." };
+		Scene deviceScenes[] = {ambush};
+		
+		String ambushDesc = "YOU HEAR AN ARCANE WEAPON CHARGING UP";
+		String ambushChoices[] = { "...", "!" };
+		Scene ambushScenes[] = {ambushWin, ambushFail};
+		ambush.addAutoAdvance(3, 1);
+		
+		String ambushFailDesc = "A red flash pierces through your chest and you collapse on the ground. You cannot see"
+							  + "; it seems that your assailant has enveloped you in magical darkness.";
+		String ambushFailChoices[] = { "Pain...", "Turn to face the darkness." };
+		Scene ambushFailScenes[] = {ambushBad};
+		
+		String ambushBadDesc = "The last thing you see is two red eyes and a mechanical voice. \"Poor little guardsman, unveiling"
+						   + " the enemy underground, when the true threat was that among their own ranks.\"";
+		String ambushBadChoices[] = {"Restart?", "Yes."};
+		Scene ambushBadScenes[] = {intro1};
+		
+		Scene aWNext[] = {ambushWin1};
+		ambushWin.setNextScenes(aWNext);
+		ambushWin.addAutoAdvance(2, 0);
+		
+		Scene aW1Next[] = {ambushWin2};
+		ambushWin1.setNextScenes(aW1Next);
+		ambushWin1.addAutoAdvance(2, 0);
+		
+		Scene aW2Next[] = {downWith};
+		ambushWin2.setNextScenes(aW2Next);
+		ambushWin2.addAutoAdvance(2, 0);
+		
+		String downWithDesc = "Believe it or not, you know who this is. They're your regiment's artificer, an arcane engineer who"
+							+ " takes charge of information collection and technology. And, evidently, it seems that this one has"
+							+ " engaged in dark dealings with the enemy. Their eyes glow red and green; they cackle as they gradually"
+							+ " slip away. \"Foolish, meddling guardsman...Ha...You may have struck me down, but your efforts are all"
+							+ " for naught! I will make sure you go down with me!\"";
+		String downWithChoices[] = { "One of their mechanical arms reaches for something!", "Grab it!" };
+		Scene downWithScenes[] = {imminentBoom};
+		
+		String imminentBoomDesc = "Your quick thinking allows you to tackle the corrupted artificer, but not before they are able to "
+								+ "take out their own crystal orb. Lightning crackles and jumps toward the machine. You know that with"
+								+ " that energy, this whole cave system is set to blow. Tackling down the artificer has interrupted the"
+								+ " process enough to buy you a little time to do one last thing...";
+		String imminentBoomChoices[] = { "Their orb seems immune to the machine's mitigation...", "Connect the orbs." };
+		Scene imminentBoomScenes[] = {orbSuccess};
+		
+		String orbSuccessDesc = "You hastily set a connection between the former artificer's orb and your own. Able to access the information"
+							  + " inside yours, the artificer's orb is able to establish a connection with home and send the message. As the"
+							  + " machine growls even more aggressively, you are prepared to accept your fate. You lay down next to the nearest"
+							  + " rock to rest...";
+		String orbSuccessChoices[] = { "But one last transmission comes through the orb...", "Listen in..." };
+		Scene orbSuccessScenes[] = {orbSuccess1};
+		
+		String orbSuccess1Desc = "It's the princess. It seems she wanted to reply to you, face to face. \"Well done soldier. This information"
+							   + " will prove useful to the war effort. But wait, where is your regiment?\" You give a weak reply: \"Died with"
+							   + " honor, ma'am. And perhaps...One dishonerably.\" With a pause, the princess gives you one last order. \"I"
+							   + " understand. Rest easy now.\" You close your eyes, and accept this end.";
+		String orbSuccess1Choices[] = {"Restart?", "Yes."};
+		Scene orbSuccess1Scenes[] = {intro1};
 		//Caught
 		String fleeCaughtDesc = "A red arcane blast trips up your feeble attempt to escape. You crash onto the freezing sands.";
 		String fleeCaughtChoices[] = { "That hurt...", "Look over your shoulder." };
@@ -167,16 +240,48 @@ public class CrystalStoryline
 		fall1.createSceneFrame(folderPath + "fall1.png");
 		fall2.createSceneFrame(folderPath + "fall2.png");
 		undertale.createSceneFrame(folderPath + "undertale.png", undertaleDesc, undertaleChoices, undertaleScenes);
+		setToCaveColors(undertale, 1);
+		device.createSceneFrame(folderPath + "device.png", deviceDesc, deviceChoices, deviceScenes);
+		setToCaveColors(device, 1);
+		ambush.createSceneFrame(folderPath + "ambush.png", ambushDesc, ambushChoices, ambushScenes);
+		setToCaveColors(ambush, 1);
+		
+		ambushFail.createSceneFrame("empty", ambushFailDesc, ambushFailChoices, ambushFailScenes);
+		setToCaveColors(ambushFail, 0);
+		ambushBad.createSceneFrame(folderPath + "ambushBad.png", ambushBadDesc, ambushBadChoices, ambushBadScenes);
+		setToCaveColors(ambushBad, 1);
+		
+		ambushWin.createSceneFrame(folderPath + "ambushWin.png");
+		ambushWin1.createSceneFrame(folderPath + "ambushWin1.png");
+		ambushWin2.createSceneFrame(folderPath + "ambushWin2.png");
+		downWith.createSceneFrame(folderPath + "downWith.png", downWithDesc, downWithChoices, downWithScenes);
+		setToCaveColors(downWith, 1);
+		imminentBoom.createSceneFrame(folderPath + "imminentBoom.png", imminentBoomDesc, imminentBoomChoices, imminentBoomScenes);
+		setToCaveColors(imminentBoom, 1);
+		orbSuccess.createSceneFrame(folderPath + "orbSuccess.png", orbSuccessDesc, orbSuccessChoices, orbSuccessScenes);
+		setToCaveColors(orbSuccess, 1);
+		orbSuccess1.createSceneFrame(folderPath + "orbSuccess1.png", orbSuccess1Desc, orbSuccess1Choices, orbSuccess1Scenes);
+		setToCaveColors(orbSuccess1, 1);
 		
 		//Caught
 		fleeCaught.createSceneFrame(folderPath + "fleeCaught.png", fleeCaughtDesc, fleeCaughtChoices, fleeCaughtScenes);
 		withThem.createSceneFrame(folderPath + "withThem.png", withThemDesc, withThemChoices, withThemScenes);
 		fleeBetrayal.createSceneFrame(folderPath + "fleeBetrayal.png", fleeBetrayalDesc, fleeBetrayalChoices, fleeBetrayalScenes);
 		fleeBad.createSceneFrame(folderPath + "fleeBad.png", fleeBadDesc, fleeBadChoices, fleeBadScenes);
+		setToCaveColors(fleeBad, 1);
 		
 		/***********************************************************************************************************/
 		
 		//Start game at introduction
 		intro1.getFrame().setVisible(true);
 	}
+	
+	private static void setToCaveColors(Scene scene, int textComponent) 
+	{
+		Color caveColor = new Color(9, 3, 49);
+		scene.getFrame().getContentPane().setBackground(caveColor);
+		scene.getFrame().getContentPane().getComponent(textComponent).setForeground(Color.white); //Set text as white
+	}
 }
+
+
